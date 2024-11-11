d232aa31fc6f63efd46cf826b4ff58dc784aed31
Justin Dennison <justin1dennison@gmail.com>
2024-11-11 17:38:58 -0500

    chore: update site name from blog to personal for routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://justin1dennison.github.io",
-  base: "/blog",
+  base: "/personal",
   integrations: [mdx(), sitemap()],
 });
